82d33a0caed4347cb79bec5d46bf571d9f83ce63
Larissa Gabrielle Fagundes Andrade <larissa.gabrielle@alunoescolassociais.org.br>
2024-10-31 07:13:51 -0300

    mude os valores de cada moeda para o valor que está hoje.

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
  
     //Obter valores de entrada informados pelo usuário
     const valor = parseFloat(document.getElementById('amount').value);
-    const daMoeda = document.getElementById('daMoeda').value;
-    const paraMoeda = document.getElementById('paraMoeda').value;
+    const daMoeda = document.getElementById('from-currency').value;
+    const paraMoeda = document.getElementById('to-currency').value;
  
     //Definir taxas de câmbio fixas
     const exchangeRates = {
-        USD: { BRL: 5.70, EUR: 0.93 },
-        BRL: { USD: 0.18, EUR: 0.16 },
-        EUR: { USD: 1.08, BRL: 6.16 }
+        USD: { BRL: 5.76, EUR: 0.92 },
+        BRL: { USD: 0.17, EUR: 0.16 },
+        EUR: { USD: 1.09, BRL: 6.18 }
     };
  
     //Conversão simples de moedas
@@ -21,7 +21,7 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
         valorConvertido = valor * exchangeRates[daMoeda][paraMoeda];
     }
  
-    const conversao = document.getElementById('conversao');
+    const conversao = document.getElementById('result');
     conversao.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${paraMoeda} `;
    
 });
